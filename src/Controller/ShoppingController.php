6cd8ccad14c0359001a9d20e07de9059d9f2f028
cedric42122 <48717607+cedric42122@users.noreply.github.com>
2019-10-20 17:08:46 +0200

    Ajout panier pour achat de prestation + début d'intégration de Stripe

--- src/Controller/ShoppingController.php
+++ src/Controller/ShoppingController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ServiceDelivery;
 use App\Form\ServicesType;
+use App\Service\Cart\CartService;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,7 +36,7 @@ class ShoppingController extends AbstractController
 
                 $imgFile->move(dirname(__FILE__) . "/../../public/uploads/", $imgFileName);
 
-                $service->setPicture("/uploads/".$imgFileName);
+                $service->setPicture("/uploads/" . $imgFileName);
             }
 
             $manager->persist($service);
@@ -48,5 +49,53 @@ class ShoppingController extends AbstractController
             'controller_name' => 'ShoppingController',
             'createServiceForm' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/cart", name="cart_index")
+     */
+    public function cartIndex(CartService $cartService)
+    {
+
+        return $this->render('shopping/cart.html.twig', [
+            'items' => $cartService->getFullCart(),
+            'total' => $cartService->getTotal()
+        ]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     */
+    public function cartAdd($id, CartService $cartService)
+    {
+        $cartService->add($id);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    /**
+     * @Route("/cart/remove/{id}", name="cart_remove")
+     */
+    public function cartRemove($id, CartService $cartService)
+    {
+        $cartService->remove($id);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    /**
+     * @Route("/payment", name="payement")
+     */
+    public function paymentService(Request $request)
+    {
+        \Stripe\Stripe::setApiKey('pk_test_8Lgfp4jEhIIkMPlu2oiM8ToN00zRv85lFd');
+    
+        \Stripe\Charge::create([
+            'amount' => 2000,
+            'currency' => 'eur',
+            'source' => $request->request->get('stripeToken'),
+            'description' => 'Paiement de test',
+        ]);
+        return $this->render('shopping/payment.html.twig');
     }
 }
---
+++ src/Service/Cart/CartService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Service\Cart;
+
+use App\Repository\ServiceDeliveryRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+class CartService {
+
+protected $session;
+protected $serviceDeliveryRepository;
+
+public function __construct(SessionInterface $session, ServiceDeliveryRepository $serviceDeliveryRepository)
+{
+    $this->session = $session;
+    $this->serviceDeliveryRepository = $serviceDeliveryRepository;
+}
+
+    public function add(int $id) 
+    {
+        $cart = $this->session->get('cart', []);
+
+        // Si le produit est déjà selectionné on en rajoute 1
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $this->session->set('cart', $cart);
+
+    }
+
+    public function remove(int $id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if(!empty($cart[$id])) {
+            unset($cart[$id]);
+        }
+
+        $this->session->set('cart', $cart);
+
+
+    } 
+
+    public function getFullCart() : array
+    {
+        $cart = $this->session->get('cart', []);
+
+        $cartWithData = [];
+
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'service' => $this->serviceDeliveryRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+        return $cartWithData;
+    } 
+
+    public function getTotal() : float
+    {
+        $total = 0;
+
+        foreach($this->getFullCart() as $item) {
+            $total += $item['service']->getPrice() * $item['quantity'];
+        }
+
+        return $total;
+
+    } 
+
+
+}
